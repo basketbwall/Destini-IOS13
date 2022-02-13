8c2bab6be440c216d9354e757f151632a61f9d07
William <tuj10861@temple.edu>
2022-02-13 17:58:00 -0500

    added code from old repository into new one

---
+++ Destini-iOS13/Controller/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyBrain = StoryBrain()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    @IBAction func choiceMade(_ sender:UIButton) {
+        let userAnswer = sender.currentTitle!
+        
+        if (userAnswer == storyBrain.getStoryChoice1()) {
+            storyBrain.nextStory(destination: storyBrain.getChoice1Destination())
+        } else {
+            storyBrain.nextStory(destination: storyBrain.getChoice2Destination())
+        }
+        
+        //update the UI
+        updateUI();
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getStoryChoice1(), for: UIControl.State.normal)
+        choice2Button.setTitle(storyBrain.getStoryChoice2(), for: UIControl.State.normal)
+    }
+
+
+}
+
